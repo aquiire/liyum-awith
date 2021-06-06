be14ba8dac5a7265403dd925e74e2576b58d50ef
aquire <aquire@outlook.com>
2021-06-06 10:28:01 +0530

    add sync and minor/major changes to subs
    There were some sync problems with the original timings so I made a
    small script to do some changes. Some idiotic mistakes have been edited
    out as well as some minor nuances of the subtitiles.

---
+++ sync.py
@@ -0,0 +1,18 @@
+import srt
+from datetime import timedelta
+
+INPUT = "You've Got Mail (si).srt"
+OUTPUT = "out.srt"
+START = 1411
+END = -1
+SHIFT = timedelta(milliseconds=500)
+
+with open(INPUT) as f:
+    subs = list(srt.parse(f.read()))
+
+for sub in subs[START-1:END]:
+    sub.start += SHIFT
+    sub.end += SHIFT
+
+with open(OUTPUT, 'w') as f:
+    f.write(srt.compose(subs))
